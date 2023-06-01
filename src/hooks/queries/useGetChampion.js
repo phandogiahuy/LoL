500331e43ae076ba095285f71fc25a10c43cbb36
Huy <phandogiahuy2016@gmail.com>
2023-06-01 16:05:01 +0700

    hello

--- src/hooks/queries/useGetChampion.js
+++ src/hooks/queries/useGetChampion.js
@@ -5,7 +5,7 @@ import { GET_CHAMPION } from "../../constant/queryKey";
 
 const getChamp = async () => {
   const { data } = await axios.get(
-    `http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion.json?fbclid=IwAR0LYJt8ZOxltNBJvYdctabsRQX8kSsIGSBpdU2FCCICCQZz9TSOJ5F2D04`
+    `https://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion.json?fbclid=IwAR0LYJt8ZOxltNBJvYdctabsRQX8kSsIGSBpdU2FCCICCQZz9TSOJ5F2D04`
   );
   return data;
 };
--- src/hooks/queries/useGetItemFromApi.js
+++ src/hooks/queries/useGetItemFromApi.js
@@ -5,7 +5,7 @@ import { GET_ITEM } from "../../constant/queryKey";
 
 const getItem = async () => {
   const { data } = await axios.get(
-    `http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/item.json`
+    `https://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/item.json`
   );
   return data.data;
 };
